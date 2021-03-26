79930cabd78deecdfa28ebc53d9d97fd3beea72d
kazzi09 <72947115+kazzi09@users.noreply.github.com>
2021-03-26 18:40:13 +0100

    2de versie arguments

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
 # Do not modify these lines
 __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
+def greet(name, greeting_template='Hello, <name>!'):
+    greet = greeting_template.replace('<name>', name)
+    return greet
 
-# Add your code after this line
-# Part 1: Greet Template
-def greet(name, greetings='Hello, <name>!'):
-       print(f'{greetings}{name}!')
-greet('Kazim', 'Hello, ')
 # Part 2: Force
 def force(mass, body='earth'):
-    d = {
-        'sun': 274.0,
-        'jupiter': 24.92,
-        'neptune': 11.15, 
-        'saturn': 10.44, 
-        'earth': 9.798,
-        'uranus': 8.87, 
-        'venus': 8.87, 
-        'mars': 3.71,
+    planets_gravity = {
+        'sun': 274,
+        'jupiter': 24.9,
+        'neptune': 11.15,
+        'saturn': 10.4,
+        'earth': 9.8,
+        'uranus': 8.9,
+        'venus': 8.9,
+        'mars': 3.7,
         'mercury': 3.7,
-        'moon': 1.62, 
-        'pluto': 0.58
+        'moon': 1.6,
+        'pluto': 0.6
     }
-    i = round((mass * d[body]), 1)
-    return i
-print(force(0.1, 'earth'))
+    force = mass * planets_gravity[body]
+    return force
+
 # Part 3: Gravity
-def pull(m1, m2 , d):
-    G = 6.674  * (10 ** -11)
-    x = G * ((m1 * m2) / d ** 2)
-    return x
-print(pull(800, 1500, 3))
+def pull(m1, m2, d):
+    pull = (6.674 * (10 ** -11)) * ((m1 * m2) / d **2)
+    return pull
